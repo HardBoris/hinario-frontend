01c457a431d7975d64887ae52127255a0d1b931e
HardBoris <bgaibor03@gmail.com>
2023-02-23 21:18:57 -0400

    feat: hymnal open

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,13 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { Login } from "../pages/Login";
-import { SignUp } from "../pages/SignUp";
+import { Home } from "../pages/Home";
+// import { Login } from "../pages/Login";
+// import { SignUp } from "../pages/SignUp";
 
 export const PublicRoutes = () => {
   return (
     <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="signup" element={<SignUp />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Home />} />
+      {/* <Route path="login" element={<Login />} /> */}
+      {/* <Route path="signup" element={<SignUp />} /> */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
